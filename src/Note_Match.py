996340a8ded10a13ece5b0dd9cd7b7c9cf2ea00b
dengjincs <13022555585@qq.com>
2019-08-03 23:04:23 +0800

    相似性比较

--- src/Note_Match.py
+++ src/Note_Match.py
@@ -1,19 +1,49 @@
 import numpy as np
 import librosa
+import madmom
 from mido import MidiFile
 from scipy.signal import argrelextrema
 from math import log10
+from scipy.stats.stats import pearsonr
 
+def Onset(y,margin):
+    CNNOnset = madmom.features.onsets.CNNOnsetProcessor()
+    RNNOnset = madmom.features.onsets.RNNOnsetProcessor()
+    # superflux = madmom.features.onsets.superflux(spectrogram=spec)
+    # complexflux = madmom.features.onsets.complex_flux(spectrogram=spec)
+    # percussive = librosa.effects.percussive(y)
 
-def MCQS(y,sr,from_note='A0',to_note='C8',max_n = 5):
+    cnn_onset = CNNOnset(y)
+    rnn_onset = RNNOnset(y)
+    #onset = (cnn_onset + rnn_onset) / 2
+    onset = cnn_onset
+    index1 = int(len(cnn_onset)/3) -1
+    index2 = int(len(cnn_onset)*2/3) - 1
+    target = cnn_onset[index1:index2]#中间窗口
+    max_index, max_onset = 0, 0
+    for index in range(index1+1):
+        if target[index] > max_onset:
+            max_index = index
+            max_onset = target[index]
+    if max_index == 0 and max_onset < cnn_onset[index1-1]:
+        max_onset = 0
+        max_index = -1
+    if max_index == index1 - 1 and max_onset < cnn_onset[index2+1]:
+        max_onset = 0
+        max_index = -1
+    if max_onset < margin:
+        max_index = -1
+    return max_index
+
+def MCQS(y,sr,from_note=21,to_note=108,max_n = 12):
     #caculate the CQT spectrum
     #retaining only the local maximum pitch
     #restrict the top max_n pitch
     #return the modified CQT Spectrum
 
-    n_bins = librosa.note_to_midi(to_note) - librosa.note_to_midi(from_note)
-    C = np.abs(librosa.cqt(y, sr=sr, bins_per_octave=12,
-                           window='hamm', fmin=librosa.note_to_hz(from_note),
+    n_bins = to_note - from_note
+    C = np.abs(librosa.cqt(y, sr=sr,hop_length=256, bins_per_octave=12,
+                           window='hamm', fmin=librosa.midi_to_hz(from_note),
                            n_bins=n_bins))
     A2dB = librosa.amplitude_to_db(C, ref=np.max)
     frame_pitch = A2dB.transpose()
@@ -35,24 +65,20 @@ def MCQS(y,sr,from_note='A0',to_note='C8',max_n = 5):
     print("Normlized:{}".format(MCQS.shape))
     T_MCQS = MCQS.transpose()
 
-    return A2dB,T_MCQS
+    return T_MCQS
 
-def SDVs(MCQS,type=3,onset=-1,threshold=3):
+def SDVs(MCQS,type=3,onset=-1,onste_len = 30):
     # Spectrum Difference Vectors (SDVs) ψi
     # dAi is the difference between these two vectors
     T_MCQS = MCQS.transpose()
+    bias = int(len(T_MCQS) / 3)
+    index = int(onset * len(T_MCQS) / onste_len)
     if onset != -1:
-        if onset-threshold > 0:
-            V_before_onset = T_MCQS[onset-threshold:onset]
-        else:
-            V_before_onset = T_MCQS[0:onset]
-        if onset+threshold < len(T_MCQS) - 1:
-            V_after_onset = T_MCQS[onset:onset+threshold]
-        else:
-            V_after_onset = T_MCQS[onset:len(T_MCQS) - 1]
+        V_before_onset = T_MCQS[index:index+bias]
+        V_after_onset = T_MCQS[index+bias:index+2*bias]
 
-        Mean_before = T_MCQS[0]
-        Mean_after = T_MCQS[len(T_MCQS) - 1]
+        Mean_before = T_MCQS[index]
+        Mean_after = T_MCQS[index]
 
         dA = T_MCQS[0] - T_MCQS[0]
         dAmax = 0
@@ -85,12 +111,16 @@ def SDVs(MCQS,type=3,onset=-1,threshold=3):
 def SPVs(midi='../piano/chopin_nocturne_b49.mid'):
     mid = MidiFile(midi)
     note_on_count = 0
-    time = 0
-    onset_count = 1
-    onset_index = 0
-    min_note = 108
-    max_note = 0
+    time = 0 # onset time
+    onset_count = 1 # onset count
+    onset_index = -1 #
+    min_note = 108 # minimum note midi
+    max_note = 0 # maxmum note midi
+    max_concurrence = 1 #max concurrence count in whole concurrence
+    cur_concurrence = 1 #concurrence count in every concurrence
     for msg in mid:
+        if msg.type != 'note_on' and msg.type != 'note_off':
+            continue
         if msg.type == "note_on":
             if msg.note > max_note:
                 max_note = msg.note
@@ -99,25 +129,32 @@ def SPVs(midi='../piano/chopin_nocturne_b49.mid'):
         pre_time = time
         if msg.time != 0:
             time += msg.time
-        if msg.type == "note_on" and pre_time != time:
-            onset_count += 1
-    note_range = max_note - min_note + 25
-    print(min_note,max_note)
-    concurrence = np.zeros((onset_count, note_range), dtype=int)
+        if cur_concurrence > max_concurrence:
+            max_concurrence = cur_concurrence
+        if msg.type == "note_on":
+            if pre_time != time:
+                onset_count += 1
+                cur_concurrence = 1
+            else:
+                cur_concurrence += 1
+
+    note_range = max_note - min_note + 1
+    #print(min_note,max_note,note_range)
+    Concurrence = np.zeros((onset_count, note_range), dtype=int)
     SPV1 = np.zeros((onset_count, note_range), dtype=int)
     SPV2 = np.zeros((onset_count, note_range), dtype=int)
     SPV3 = np.zeros((onset_count, note_range), dtype=int)
 
     time = 0
     for msg in mid:
+        if msg.type != 'note_on' and msg.type != 'note_off':
+            continue
         pre_time = time
         if msg.time != 0:
             time += msg.time
-        if pre_time != time:
-            print('\n')
         if msg.type == "note_on":
             if note_on_count == 1:
-                print(onset_index,time,msg)
+                #print(onset_index,time,msg)
                 note_on_count += 1
                 continue
             if pre_time != time:
@@ -128,26 +165,53 @@ def SPVs(midi='../piano/chopin_nocturne_b49.mid'):
             [19] PerfectFifth
             [24] Third Octave
             """
-            concurrence[onset_index][msg.note - min_note + 0] = 1
+
+            Concurrence[onset_index][msg.note - min_note + 0] = 1
+            print("msg.note:{} index:{}".format(msg.note,msg.note - min_note))
             for over_tune in [0,12]:
                 index = msg.note - min_note + over_tune
-                if index <= max_note:
+                if index < note_range:
                     SPV1[onset_index][index] = 1
             for over_tune in [0,12,19]:
                 index = msg.note - min_note + over_tune
-                if index <= max_note:
+                if index < note_range:
                     SPV2[onset_index][index] = 1
             for over_tune in [0, 12, 19, 24]:
                 index = msg.note - min_note + over_tune
-                if index <= max_note:
+                if index < note_range:
                     SPV3[onset_index][index] = 1
-            print(onset_index, time, msg)
-    return min_note,max_note,concurrence,SPV1,SPV2,SPV3
+    return min_note,max_note,max_concurrence,Concurrence,SPV1,SPV2,SPV3
 
+def Similarity(sdv,concurrence,spv1,spv2,spv3,k):
+    correlation1 = pearsonr(sdv, concurrence[k])[0]
+    correlation2 = pearsonr(sdv, spv1[k])[0]
+    correlation3 = pearsonr(sdv, spv2[k])[0]
+    correlation4 = pearsonr(sdv, spv3[k])[0]
+    return max(correlation1,correlation2,correlation3,correlation4)
 
-_,_,concurrence,spv1,spv2,spv3 = SPVs()
-for i in range(100):
-    print(i,'\n',spv3[i])
+def Ita(audio_onset2,audio_onset1,audio_onset,
+        score_onset2,score_onset1,score_onset,
+        j,j0,a = 0.1,):
+    # ita means the local tempo coefficient
+    # m1 & m2 are the slopes of the latest two segments
+    # j is the index of the concurrence
+    # j0 is the index of the previous matched concurrence
+    # delta_j is a penalty to avoid injurious score jumps & onset insertions
+    delta_j = j - j0 - 1
+    m1 = (score_onset - score_onset1) / (audio_onset - audio_onset1)
+    m2 = (score_onset1 - score_onset2) / (audio_onset1 - audio_onset2)
+    if m1/m2 < 4:
+        ita = a * (1 - abs(log10(m1/m2) / log10(2)) - delta_j)
+    else:
+        ita = -1
+    return ita
+#sdv = MCQS(y,sr,min_note,max_note,max_concurrence)
+#S = Similarity(sdv,Concurrence,spv1,spv2,spv3)
 
+a = [1,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,1]
+b = [0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1]
+c = pearsonr(a,b)
+print(c)
+print(librosa.note_to_midi('A0'))
 
 
--- src/main.py
+++ src/main.py
@@ -1,6 +1,4 @@
-import librosa.display
-import matplotlib.pyplot as plt
-from src.Onset import *
+
 from src.Note_Match import *
 
 piano = '../piano/MAPS_ISOL_CH0.3_F_AkPnBcht.wav'
@@ -9,76 +7,47 @@ piano_midi = '../piano/MAPS_ISOL_CH0.3_F_AkPnBcht.mid'
 chopin_wav = '/Code/PyCharm/LivePiano/piano/chopin_nocturne_b49.wav'
 chopin_midi = '/Code/PyCharm/LivePiano/piano/chopin_nocturne_b49.mid'
 y,sr = librosa.load(chopin_wav,None)
-y = y[:10*sr]
+y = y[:50*sr]
+margin = 0.9
 
-block_dur = 0.2#s
-block_len = int(block_dur * sr)
-n_block = int(len(y) / block_len)
+block = 0.1#s
+n_block = int((len(y)/sr)/block)
+buff = np.zeros(int(3 * block * sr))
 
-count_onset = 0
-pre_onset_index = 0
-pre_onset = 0
-margin = 0.45
+min_note,max_note,max_concurrence,concurrence,spv1,spv2,spv3 = SPVs(midi=piano_midi)
+print("min_note:",min_note)
+print("max_note:",max_note)
+print("max_concurrence:",max_concurrence)
+print("concurrence:\n")
+count = 0
+for i in spv3:
+    print(i,count,len(i))
+    print()
+    count += 1
+print("SPV1:\n",spv1)
+print("SPV2:\n",spv2)
+print("SPV3:\n",spv3[0],len(spv3[0]))
+onset_count = -1
 
-for i in range(n_block):
-    block = y[i*block_len:(i+1)*block_len]
-    index,onset = Onset(block)
-    print(index,onset)
-
-
-    if index == block_dur / 0.01 - 1:
-        # the local maxmum at the end of a block
-        # read a new block
-        i += 1
-        count_onset += 1
-        # cache the current result
-        pre_onset_index, pre_onset = index, onset
-        block = y[i * block_len:(i + 1) * block_len]
-        index, onset = Onset(block)
-        if pre_onset == onset and pre_onset_index == index:
-            index, onset = pre_onset_index, pre_onset
-        else:
-            pre_onset_index, pre_onset = index, onset
-            continue
-
-    elif index == 0 and pre_onset == onset:
-        # the local maxmum at the begain of a block
-        # compere with the pre block
-        index, onset = pre_onset_index, pre_onset
-
-
+for i in range(n_block-3):
+    if i == 0:
+        buff[int(block * sr):int(block * sr * 2 - 1)] = y[:int(block * sr - 1)]
+    elif i == 1:
+        buff[:int(block * sr * 2 - 1)] = y[:int(block * sr * 2 - 1)]
     else:
-        if onset > margin:
-            count_onset += 1
+        buff = y[int((i - 1) * block * sr):int((i + 1) * block * sr)]
+    onset = Onset(buff,margin=margin)
+    if onset == -1:
+        continue
+    else:
+        onset_count +=1
+        mcqs = MCQS(y=buff,sr=sr,
+                    from_note=min_note,to_note=max_note+1,max_n=max_concurrence)
+        sdv = SDVs(mcqs,onset=onset)
+        similarity = Similarity(sdv,concurrence,spv1,spv2,spv3,onset_count)
+        print("sdv:",sdv)
+        print("spv3:",spv3[onset_count])
+        print("onset:",onset,"similarity:",similarity)
 
-    if onset > margin:
-        pre_onset_index, pre_onset = index, onset
-        onset_time = i * block_dur + (index) / 100
-        # every 10ms as a frame to get a result
-        print("{}-{}:{}-{}".format(count_onset, index, onset,onset_time))
-
-CNNOnset = madmom.features.onsets.CNNOnsetProcessor()
-RNNOnset = madmom.features.onsets.RNNOnsetProcessor()
-cnn_onset = CNNOnset(y)
-rnn_onset = RNNOnset(y)
-onset = (cnn_onset + rnn_onset)/2
-plt.plot(onset)
-plt.show()
-
-
-A2dB,T_MCQS = MCQS(y,sr)
-plt.subplot(2,1,2)
-librosa.display.specshow(T_MCQS,sr=sr,x_axis='time',y_axis='cqt_note')
-plt.colorbar(format = '%+3.0f dB')
-plt.title("Modified CQT Spectrum")
-plt.tight_layout()
-
-plt.subplot(2,1,1)
-librosa.display.specshow(A2dB,sr=sr,x_axis='time',y_axis='cqt_note')
-plt.colorbar(format = '%+3.0f dB')
-plt.title("Constant-Q power spectrum")
-plt.tight_layout()
-
-#plt.show()
 
 
